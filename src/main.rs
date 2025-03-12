f74bdcf711e7a8d3721f8caa01c45d09c1afe78b
Toshihiro YAMAGUCHI <yamaguchi.toshihiro@gmail.com>
2025-03-12 09:14:28 +0900

    Add obiwan module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+mod obiwan;
+
 #[tokio::main]
 async fn main() {
-    let app = axum::Router::new().route("/", axum::routing::get(|| async { "Hello, World!" }));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    obiwan::start().await;
 }
---
+++ src/obiwan.rs
@@ -0,0 +1,5 @@
+pub async fn start() {
+    let app = axum::Router::new().route("/", axum::routing::get(|| async { "Hello, World!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
