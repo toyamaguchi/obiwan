ecabfa67869336ea22a6dffbcd46b7aaf7e234a1
Toshihiro YAMAGUCHI <yamaguchi.toshihiro@gmail.com>
2025-03-12 20:50:25 +0900

    Add video players and feature to read local files

--- src/obiwan.rs
+++ src/obiwan.rs
@@ -1,4 +1,6 @@
 use axum::extract::FromRef;
+use axum::response::IntoResponse;
+use serde_derive::Deserialize;
 
 const STATIC_DIR: include_dir::Dir<'static> =
     include_dir::include_dir!("$CARGO_MANIFEST_DIR/rsc/static");
@@ -6,6 +8,11 @@ const STATIC_DIR: include_dir::Dir<'static> =
 #[derive(Clone, FromRef)]
 struct AppState {
     hb: handlebars::Handlebars<'static>,
+}
+
+#[derive(Deserialize)]
+struct PathParameter {
+    path: String,
 }
 
 async fn index(hb: axum::extract::State<handlebars::Handlebars<'_>>) -> axum::response::Response {
@@ -17,17 +24,56 @@ async fn index(hb: axum::extract::State<handlebars::Handlebars<'_>>) -> axum::re
         .unwrap()
 }
 
+async fn api_v1_file(
+    path_parameter: axum::extract::Query<PathParameter>,
+    range: Option<axum_extra::TypedHeader<axum_extra::headers::Range>>,
+) -> axum::response::Response {
+    let path_parameter = path_parameter.0;
+    let path_buf = std::path::PathBuf::from(path_parameter.path);
+
+    // check file existance
+    let file = tokio::fs::File::open(&path_buf).await.unwrap();
+    if file.metadata().await.is_err() {
+        return axum::response::Response::builder()
+            .status(axum::http::StatusCode::NOT_FOUND)
+            .body(axum::body::Body::empty())
+            .unwrap();
+    }
+
+    // create response body
+    let body = axum_range::KnownSize::file(file).await.unwrap();
+    let range = range.map(|axum_extra::TypedHeader(range)| range);
+    let mut response = axum_range::Ranged::new(range, body).into_response();
+
+    // set Content-Type
+    let mime = match path_buf.extension() {
+        Some(extension) => match extension.to_str().unwrap() {
+            ".avi" => Some("video/video/x-msvideo"),
+            ".mp4" => Some("video/mp4"),
+            _ => None,
+        },
+        None => None,
+    };
+    if let Some(mime) = mime {
+        let header_map = response.headers_mut();
+        header_map.append("Content-Type", mime.parse().unwrap());
+    }
+
+    response
+}
+
 pub async fn start() {
     let mut hb: handlebars::Handlebars<'static> = handlebars::Handlebars::new();
     hb.register_template_string("index", include_str!("../rsc/template/index.html"))
         .expect("failed to register template string: ../rsc/template/index.html");
 
-    let app_state = AppState { hb: hb };
+    let app_state = AppState { hb };
 
     let static_dir_service = tower_serve_static::ServeDir::new(&STATIC_DIR);
 
     let app = axum::Router::new()
         .route("/", axum::routing::get(index))
+        .route(r"/api/v1/file", axum::routing::get(api_v1_file))
         .nest_service("/static", static_dir_service)
         .with_state(app_state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
